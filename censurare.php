669b7648835fbc681718d2380c344c8721004dc9
adelinashahaj <adelinarucaj@icloud.com>
2023-04-17 18:01:32 +0200

    fine esercizio

---
+++ censurare.php
@@ -0,0 +1,23 @@
+
+<?php
+    $testo = $_GET['testo'];
+    $parola = $_GET['parolaCensurata'];
+    $testoCensurato = str_replace($parola,'***',$testo);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css" />
+    <title>PHP Badwords</title>
+</head>
+<body>
+<h1>Questo è il testo censurato:</h1>
+<h2><?php echo $testoCensurato ?></h2>
+<h3>Il nuovo testo è lungo: <?php echo strlen($testoCensurato);?> caratteri</h3>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="GET" action="censurare.php">
+       <textarea name="testo" >Oggi ho fatto una passeggiata al mare</textarea>
+
+        <input type="text" name="parolaCensurata" id="nome" placeholder="Inserisci la parola da cens"/>
+        <button type="submit">Invia</button>
+    </form>
+</body>
+</html>
